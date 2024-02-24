7f674f044e464e2fbaf02e44a68f113a67383959
Aryan Shrivastava <aryan1shrivastava@gmail.com>
2024-02-24 20:27:33 +0530

    bgChanger Project done

--- bgChanger/src/App.jsx
+++ bgChanger/src/App.jsx
@@ -1,35 +1,81 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState("skyblue");
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div
+      className="w-full h-screen duration-200"
+      style={{ backgroundColor: color }}
+    >
+      <div
+        className="flex justify-center items-center h-screen font-bold text-4xl text-white shadow-xl"
+        //style={"font-size: 18px"}
+      >
+        bgChanger
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="fixed flex flex-wrap justify-center bottom-16 inset-x-0 px-2">
+        <div className="fixed flex flex-wrap justify-center bg-white px-3 py-2 gap-3 shadow-lg rounded-xl">
+          <button
+            onClick={() => setColor("red")}
+            className="outline-none px-4 py-1 rounded-xl shadow-md  text-white"
+            style={{ backgroundColor: "red" }}
+          >
+            Red
+          </button>
+          <button
+            onClick={() => setColor("Green")}
+            className="outline-none px-4 py-1 rounded-xl shadow-md  text-white"
+            style={{ backgroundColor: "Green" }}
+          >
+            Green
+          </button>
+          <button
+            onClick={() => setColor("Blue")}
+            className="outline-none px-4 py-1 rounded-xl shadow-md  text-white"
+            style={{ backgroundColor: "Blue" }}
+          >
+            Blue
+          </button>
+          <button
+            onClick={() => setColor("purple")}
+            className="outline-none px-4 py-1 rounded-xl shadow-md  text-white"
+            style={{ backgroundColor: "purple" }}
+          >
+            Purple
+          </button>
+          <button
+            onClick={() => setColor("black")}
+            className="outline-none px-4 py-1 rounded-xl shadow-md  text-white"
+            style={{ backgroundColor: "black" }}
+          >
+            Black
+          </button>
+          <button
+            onClick={() => setColor("Pink")}
+            className="outline-none px-4 py-1 rounded-xl shadow-4xl  text-black"
+            style={{ backgroundColor: "pink" }}
+          >
+            Pink
+          </button>
+          <button
+            onClick={() => setColor("Orange")}
+            className="outline-none px-4 py-1 rounded-xl shadow-md  text-white"
+            style={{ backgroundColor: "Orange" }}
+          >
+            Orange
+          </button>
+          <button
+            onClick={() => setColor("Lavender")}
+            className="outline-none px-4 py-1 rounded-xl shadow-4xl  text-black"
+            style={{ backgroundColor: "Lavender" }}
+          >
+            Lavender
+          </button>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
